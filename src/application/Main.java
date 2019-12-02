7108f916a74859d04eed03eaddf3ec3c98c37ab2
CFP-PC20 <josejuan.vigil@campusfp.es>
2019-12-02 13:07:42 +0100

    Arreglado el cast

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,6 @@
 package application;
 	
-import javafx.application.Application;
+import javafx.application.Application;	
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -12,7 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			VBox root = (VBox) FXMLLoader.load(getClass().getResource("Login.fxml"));
+		
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
